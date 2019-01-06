9326508036ade13c82e4da16024aa9ea11f6b71a
Yusuke Sabi <nekomastery050313@gmail.com>
2019-01-06 23:58:04 +0900

    カメラの起動と撮影結果の遷移を実装

--- app/src/main/java/com/example/myfirstapp/CameraActivity.java
+++ app/src/main/java/com/example/myfirstapp/CameraActivity.java
@@ -1,7 +1,10 @@
 package com.example.myfirstapp;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class CameraActivity extends AppCompatActivity {
 
--- app/src/main/java/com/example/myfirstapp/MainActivity.java
+++ app/src/main/java/com/example/myfirstapp/MainActivity.java
@@ -1,26 +1,67 @@
 package com.example.myfirstapp;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
-    public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
-
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    static final int REQUEST_SHOW_RESULT = 2;
+    private Bitmap bitmap;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-    /** TODO:カメラを起動するボタン➡カメラ起動のルーティンを作る */
     /**
-     * Called when the user taps the Send button
+     * Called when the user taps the Start button
      */
     public void startCamera(View view) {
-       Intent intent = new Intent(this, CameraActivity.class);
-       startActivity(intent);
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
     }
+
+    /**
+     * カメラで画像を撮影後、結果表示画面に遷移する。
+     *
+     * @param requestCode
+     * @param resultCode
+     * @param data
+     */
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+
+            // cancelした場合
+            if (data.getExtras() == null) {
+                Log.d("debug", "cancel ?");
+                return;
+            }
+
+            bitmap = (Bitmap) data.getExtras().get("data");
+
+            if (bitmap != null) {
+                int bmpWidth = bitmap.getWidth();
+                int bmpHeight = bitmap.getHeight();
+                Log.d("debug", String.format("w= %d", bmpWidth));
+                Log.d("debug", String.format("h= %d", bmpHeight));
+            }
+
+            setContentView(R.layout.activity_camera);
+
+            ImageView screenView = findViewById(R.id.screenView);
+            screenView.setImageBitmap(bitmap);
+        }
+    }
+
+
 
 }
